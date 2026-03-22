64a206e931c8d7345ae0f1ff2d6464dbbd31d6b4
Fariez Daniel <fariezdaniel03@gmail.com>
2026-03-22 09:35:30 -0400

    implemented front end for signup

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
